e15f38ed180d31cc86f396c08dc43730a975ef2b
angielynr <106809120+angielynr@users.noreply.github.com>
2022-10-10 13:59:00 +0800

    User input event (#1)
    * Added userinputevent
    
    * Merge with master
    
    Co-authored-by: angielyn.rivera <angielyn.rivera@ph.softvision.global>

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,26 +1,40 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (data) => {
+  const [filteredYear, setFilteredYear] = useState("2022");
+  const filterChangeHandler = (selectedYear) => {
+    // console.log("expenses.js");
+    // console.log(selectedYear);
+    setFilteredYear(selectedYear);
+  };
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={data.items[0].title}
-        amount={data.items[0].amount}
-        date={data.items[0].date}
-      />
-      <ExpenseItem
-        title={data.items[1].title}
-        amount={data.items[1].amount}
-        date={data.items[1].date}
-      />
-      <ExpenseItem
-        title={data.items[2].title}
-        amount={data.items[2].amount}
-        date={data.items[2].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selectedYear={filteredYear}
+          onChangeYearFilter={filterChangeHandler}
+        />
+        <ExpenseItem
+          title={data.items[0].title}
+          amount={data.items[0].amount}
+          date={data.items[0].date}
+        />
+        <ExpenseItem
+          title={data.items[1].title}
+          amount={data.items[1].amount}
+          date={data.items[1].date}
+        />
+        <ExpenseItem
+          title={data.items[2].title}
+          amount={data.items[2].amount}
+          date={data.items[2].date}
+        />
+      </Card>
+    </div>
   );
 };
 
